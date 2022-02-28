417dbcae4d13d54fa47b5a12c645275950970a0a
David Colon <dcolonantonio@gmail.com>
2022-02-27 22:08:35 -0500

    more cleanup

--- src/context/web3/components/WalletOption.tsx
+++ src/context/web3/components/WalletOption.tsx
@@ -1,34 +1,32 @@
 import { useWeb3React } from "@web3-react/core";
 import { FC } from "react";
-import connectors, { ConnectorNames, connectorsByName } from "../connectors";
+import { ConnectorNames, connectorsByName } from "../connectors";
 import { useWeb3Connect } from "../Web3Connector";
-import cx from "classnames";
 import ConnectorButton from "./buttons/ConnectorButton";
 
 interface WalletOptionProps {
   name: string;
 }
 const WalletOption: FC<WalletOptionProps> = ({ name }) => {
-  const web3React = useWeb3React();
-  const web3Connect = useWeb3Connect();
+  const { connector, error, activate } = useWeb3React();
+  const { triedEager, activatingConnector, setActivatingConnector } = useWeb3Connect();
 
   const currentConnector = connectorsByName[name];
-  const connected = currentConnector === web3React.connector;
-  const disabled =
-    !web3Connect.triedEager || !!web3Connect.activatingConnector || connected || !!web3React.error;
+  const connected = currentConnector === connector;
+  const disabled = !triedEager || !!activatingConnector || connected || !!error;
 
   const isFallback = name === ConnectorNames.Fallback;
   return (
     <ConnectorButton
       isDisabled={disabled || isFallback}
-      isLoading={currentConnector === web3Connect.activatingConnector}
+      isLoading={currentConnector === activatingConnector}
       isFallback={isFallback}
       isConnected={connected}
       action={() => {
-        web3Connect.setActivatingConnector(currentConnector);
-        web3React.activate(connectorsByName[name], (error) => {
+        setActivatingConnector(currentConnector);
+        activate(connectorsByName[name], (error) => {
           if (error) {
-            web3Connect.setActivatingConnector(undefined);
+            setActivatingConnector(undefined);
           }
         });
       }}
